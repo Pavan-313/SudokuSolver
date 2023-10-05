58296be1e105da55c93512e0ea5c6b18583e5bad
Pavan-313 <104950761+Pavan-313@users.noreply.github.com>
2023-10-05 19:24:09 +0530

    Sudoku Solver Project using Backtracking algorithm

---
+++ SudokuSolver.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+
+public class SudokuSolver {
+    public static void main(String[] args) {
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        SudokuSolver solver = new SudokuSolver();
+        solver.solveSudoku(board);
+
+        System.out.println("Solved Sudoku:");
+        printSudoku(board);
+    }
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean isValid(char[][] board, int row, int col, char ch) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == ch || board[i][col] == ch || board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == ch) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void printSudoku(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
